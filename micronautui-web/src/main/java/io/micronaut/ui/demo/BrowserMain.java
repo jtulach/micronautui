690c1b04e0e1f23732bc13af6ea7b68c149aa83b
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2020-11-11 17:47:48 +0100

    Trying to use TeaVM to compile micronaut code

---
+++ micronautui-web/src/main/java/io/micronaut/ui/demo/BrowserMain.java
@@ -0,0 +1,10 @@
+package io.micronaut.ui.demo;
+
+public class BrowserMain {
+    private BrowserMain() {
+    }
+
+    public static void main(String... args) throws Exception {
+        Demo.onPageLoad();
+    }
+}
---
+++ micronautui-web/src/test/java/io/micronaut/ui/demo/IntegrationTest.java
@@ -0,0 +1,23 @@
+package io.micronaut.ui.demo;
+
+import net.java.html.junit.BrowserRunner;
+import net.java.html.junit.HTMLContent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Tests for behavior of your application in real systems. The {@link BrowserRunner}
+ * selects all possible presenters from your <code>pom.xml</code> and
+ * runs the tests inside of them. By default there are two:
+ * <ul>
+ *   <li>JavaFX WebView presenter - verifies behavior in HotSpot JVM</li>
+ *   <li>Bck2Brwsr presenter - runs the test in a pluginless browser</li>
+ * </ul>
+ *
+ * See your <code>pom.xml</code> dependency section for details.
+ */
+@RunWith(BrowserRunner.class)
+@HTMLContent("")
+public class IntegrationTest {
+    @Test public void testUIModelUI() {
+    }
+}
